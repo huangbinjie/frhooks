2d435898c932abe02caec9244105a318e3f08d3f
黄斌杰 <akira.binjie@gmail.com>
2019-06-16 23:48:17 +0800

    add type

--- lib/src/useRef.dart
+++ lib/src/useRef.dart
@@ -2,10 +2,11 @@ part of 'hook.dart';
 
 class RefContainer<T> {
   dynamic current;
+  RefContainer(this.current);
 }
 
-RefContainer useRef() {
-  final refStateContainer = useState(RefContainer());
+RefContainer<T> useRef<T>([T initialValue]) {
+  final refStateContainer = useState(RefContainer(initialValue));
 
   return refStateContainer.state;
 }
